3bd584296d47afdc9bebda94ad3dbc79e1c276e7
Maciej Pogorzelski <maciej.pogorzelski095@gmail.com>
2018-12-02 13:28:17 +0100

    Update seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-        factory(App\User::class, 100)->create();
-        factory(App\Post::class, 100)->create();
-        factory(App\Comment::class, 500)->create();
+        $this->call([
+            UsersTableSeeder::class,
+            PostsTableSeeder::class,
+            CommentsTableSeeder::class
+        ]);
+
     }
 }
