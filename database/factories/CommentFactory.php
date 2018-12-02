b11e2bb427698e0ba342614bd0ed2776ef8ef324
Maciej Pogorzelski <maciej.pogorzelski095@gmail.com>
2018-12-02 12:54:40 +0100

    Update seeders

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Post;
+use App\User;
 use Faker\Generator as Faker;
 
 $factory->define(App\Comment::class, function (Faker $faker) {
     return [
-      'user_id' => rand(1, 100),
-      'post_id' => rand(1, 100),
+      'user_id' => User::all()->random()->id,
+      'post_id' => Post::all()->random()->id,
       'body' => $faker->paragraphs(rand(1,3), true),
     ];
 });
--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -1,12 +1,14 @@
 <?php
 
+use App\User;
 use Faker\Generator as Faker;
 
 $factory->define(App\Post::class, function (Faker $faker) {
     return [
-        'user_id' => rand(1, 100),
+        'user_id' => User::all()->random()->id,
         'title' => $faker->sentence(),
         'body' => $faker->paragraphs(rand(5,20), true),
         //
     ];
 });
+
